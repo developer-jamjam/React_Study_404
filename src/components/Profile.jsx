b53ce1717dd56c627624c87bedaaf3142c577298
404_jamjam <jhy1025a@gmail.com>
2023-02-26 22:37:14 +0900

    React_Study_2
    react_study_2days_ing

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div className='profile'>
+            <img className='photo'
+            src='https://images.unsplash.com/photo-1544241907-f3f1f5ded15a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80' 
+            alt='avartar' />
+            <h1>Sa Gongsa</h1>
+            <p>주니어 프론트엔드 개발자</p>
+        </div>
+    );
+}
+
